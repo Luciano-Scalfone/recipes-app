37dc88a3a570e8cd08fe73dc6c8a942d337c7dd9
Luciano Scalfone Filho <luciano.scalfone@thoughtworks.com>
2023-01-17 15:50:59 -0300

    Creates home page

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+const routes = createBrowserRouter([{ path: "/", element: <Home /> }]);
+
+const Router = () => {
+  return <RouterProvider router={routes} />;
+};
+
+export default Router;
