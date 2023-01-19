21bbf03ec1485788d9f459f5db21ada15d1a92dc
Luciano Scalfone Filho <luciano.scalfone@thoughtworks.com>
2023-01-19 17:47:30 -0300

    added state to input filter

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,44 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { DebounceInput } from "react-debounce-input";
 import { SearchIcon } from "../../assets/icons";
 import { HeaderWrapper } from "./HeaderStyles";
 
 const INITIAL_STATE = {
-  filterBy: 'name',
-  value: ''
+  filterClass: "name",
+  filterBy: "",
 };
 
 export const Header = () => {
   const [filter, setFilter] = useState(INITIAL_STATE);
 
-  const onFilterChange = ({ target: { value } }) => {
+  const onFilterChange = ({ target }) => {
+    const { name, value } = target;
+
     setFilter({
       ...filter,
-      filterBy: value
+      [name]: value,
     });
-  }
+  };
 
   return (
     <HeaderWrapper>
-      <select onChange={(e) => onFilterChange(e)}>
-        <option value="name" defaultValue >name</option>
-        <option value="first-letter" >first letter</option>
-        <option value="category" >category</option>
-        <option value="area" >area</option>
-        <option value="ingredient" >ingredient</option>
+      <select name="filterClass" onChange={(e) => onFilterChange(e)}>
+        <option value="name" defaultValue>
+          name
+        </option>
+        <option value="first-letter">first letter</option>
+        <option value="category">category</option>
+        <option value="area">area</option>
+        <option value="ingredient">ingredient</option>
       </select>
-      <input type="text" placeholder="Big Mc" />
+      <DebounceInput
+        minLength={0}
+        debounceTimeout={1000}
+        name="filterBy"
+        onChange={(e) => onFilterChange(e)}
+        type="text"
+        placeholder="Big Mc"
+      />
       <button type="button">
         <SearchIcon />
       </button>
