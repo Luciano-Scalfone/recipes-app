19cfdfbaaf86af253bbd7f60b69026eff02e8cb5
Luciano Scalfone Filho <luciano.scalfone@thoughtworks.com>
2023-01-19 16:22:47 -0300

    added fheader filter boilerplate

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,18 @@
-import { HeaderWrapper } from "./HeaderStyles"
-
+import { SearchIcon } from "../../assets/icons";
+import { HeaderWrapper } from "./HeaderStyles";
 
 export const Header = () => {
-  return <HeaderWrapper>Cabeçalho</HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <select>
+        <option>test</option>
+        <option>test1</option>
+        <option>test2</option>
+      </select>
+      <input type="text" placeholder="Big Mc" />
+      <button type="button">
+        <SearchIcon />
+      </button>
+    </HeaderWrapper>
+  );
 };
--- src/Components/Header/HeaderStyles.jsx
+++ src/Components/Header/HeaderStyles.jsx
@@ -1,5 +1,46 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
+  background-color: #FFCDB5;
   grid-area: header;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & input {
+    width: 50%;
+    height: 50%;
+    border: none;
+    outline: none;
+    padding: 0 1%;
+    font-size: 2vh;
+
+    &::placeholder {
+      font-style: italic;
+      color: rgba(200, 200, 200);
+    }
+  }
+
+  & select, button {
+    background-color: rgba(245, 245, 245);
+    width: 5%;
+    height: 50%;
+    border-radius: 5px 0 0 5px;
+    border: none;
+    outline: none;
+    padding: 0 1%;
+    font-size: 2vh;
+  }
+
+  & button {
+    background-color: #FFE5AB;
+    width: 3%;
+    border-radius: 0 5px 5px 0;
+    cursor: pointer;
+    transition: filter 0.1s;
+
+    &:active {
+      filter: contrast(80%);
+    }
+  }
 `;
