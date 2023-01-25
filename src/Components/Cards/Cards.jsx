daa80f14e3782c3f1d5efe4011ad2e77a2c68024
Luciano Scalfone Filho <luciano.scalfone@thoughtworks.com>
2023-01-25 15:05:42 -0300

    refactor cards component test with tdd

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -2,7 +2,7 @@ import { CardWrapper } from "./CardStyles";
 
 export const Cards = ({ name, imageSrc }) => {
   return (
-    <CardWrapper data-testid='cards-element'>
+    <CardWrapper data-testid="cards-element">
       <img src={imageSrc} alt={`${name} meal`} />
       <h2>{name}</h2>
     </CardWrapper>
--- src/Components/Cards/Cards.test.jsx
+++ src/Components/Cards/Cards.test.jsx
@@ -15,29 +15,40 @@ describe("Cards Component", () => {
     getText = getByText;
   });
 
+  test('Should be defined', () => {
+    const CardsElement = render(<Cards />);
+
+    expect(CardsElement).toBeDefined();
+    expect(CardsElement).not.toBeNull();
+  });
+
   test("Should have a div element", () => {
     const divElement = CardsElement.querySelector("div");
 
     expect(divElement).toBeInTheDocument();
   });
 
-  test("Should have an img element like first child position in div element", () => {
+  test("Should have an img element like first child in div element", () => {
     const imgElement = CardsElement.querySelector("div img:first-child");
 
-    expect(imgElement).toBeDefined();
+    expect(imgElement).toBeInTheDocument();
   });
 
   test("Should have a h2 element like last child position in div element", () => {
     const h2Element = CardsElement.querySelector("div h2:last-child");
 
-    expect(h2Element).toBeDefined();
+    expect(h2Element).toBeInTheDocument();
   });
 
-  test("If Cards component rendered elements with props", () => {
+  test("Should pass props.name to h2 element", () => {
     const nameProps = getText("some_name");
+
+    expect(nameProps).toBeInTheDocument();
+  });
+
+  test("Should pass props.imageSrc to image soucer attribute", () => {
     const imageSrc = CardsElement.querySelector('[src="some_url"]');
 
     expect(imageSrc).toBeInTheDocument();
-    expect(nameProps).toBeInTheDocument();
   });
 });
