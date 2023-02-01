8d73c5f72ea3416712d775b17d80c1435d78eba0
A81435269g <lukaskechi7@gmail.com>
2023-02-01 16:23:16 -0300

    Added tests to do h1 and image with the correct values

--- src/pages/MealsDetails/MealsDetails.jsx
+++ src/pages/MealsDetails/MealsDetails.jsx
@@ -5,18 +5,23 @@ import { fetchById } from "../../services/fetchs";
 
 export const MealsDetails = () => {
   const urlParameter = useParams();
-  const [meal, setMeal] = useState();
+  const [meal, setMeal] = useState({});
 
   useEffect(() => {
     (async () => {
       const data = await fetchById(urlParameter.id);
-      setMeal(data);
-    })();
-  }, []);
 
+      setMeal({
+        image: data[0].strMealThumb,
+        name: data[0].strMeal,
+      });
+    })();
+  }, [urlParameter.id]);
+  
   return (
     <div className="container" data-testid="meals-details">
-      {urlParameter.id}
+      <img src={meal.image} alt="Meal" />
+      <h1>{meal.name}</h1>
     </div>
   );
 };
--- src/pages/MealsDetails/MealsDetails.test.jsx
+++ src/pages/MealsDetails/MealsDetails.test.jsx
@@ -1,10 +1,53 @@
 import { MealsDetails } from "./MealsDetails";
 import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
 describe("Meals Page", () => {
-  test("Should have a element with testid 'meals-details'", () => {
-    render(<MealsDetails />);
+  const DATA = [
+    {
+      strMeal: "big_mac",
+      strMealThumb: "big_mac_image",
+    },
+  ];
 
+  beforeEach(async () => {
+    const mockFunction = require("../../services/fetchs");
+    mockFunction.fetchById = jest.fn().mockReturnValue(DATA);
+
+    await act(async () => render(<MealsDetails />));
+  });
+
+  test("Should have a element with testid 'meals-details'", () => {
     expect(screen.getByTestId("meals-details")).toBeInTheDocument();
+  });
+
+  test("Should have an image component in element with testid 'meals-details'", () => {
+    const continerElement = screen.getByTestId("meals-details");
+    const imgElement = continerElement.getElementsByTagName("img")[0];
+
+    expect(imgElement).toBeInTheDocument();
+  });
+
+  test("Should have a property src in img with correct URL", () => {
+    const continerElement = screen.getByTestId("meals-details");
+    const propSrcFromImg = continerElement
+      .getElementsByTagName("img")[0]
+      .getAttribute("src");
+
+    expect(propSrcFromImg).toEqual(DATA[0].strMealThumb);
+  });
+
+  test("Should have a h1 component in element with testid 'meals-details'", () => {
+    const continerElement = screen.getByTestId("meals-details");
+    const h1Element = continerElement.getElementsByTagName("h1")[0];
+
+    expect(h1Element).toBeInTheDocument();
+  });
+
+  test("Should have the meal name in h1 component", () => {
+    const continerElement = screen.getByTestId("meals-details");
+    const h1Element = continerElement.getElementsByTagName("h1")[0];
+
+    expect(h1Element.textContent).toEqual(DATA[0].strMeal);
   });
 });
